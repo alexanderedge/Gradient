dc0cd2b1b2243595d3b4cd5de81edf200e79b7e5
Alexander Edge <alex@alexedge.co.uk>
2014-02-24 11:35:38 +0000

    Add rotation back in

--- Gradient/GRDGradientLayer.m
+++ Gradient/GRDGradientLayer.m
@@ -31,20 +31,56 @@ static NSTimeInterval const kAnimationDuration = 0.3f;
     return self;
 }
 
+static CGFloat GRDRandomAngle() {
+    return (CGFloat)arc4random_uniform(1000) / 1000.f * M_PI;
+}
+
+static CGPoint GRDStartPointForAngle (CGFloat angle){
+    return CGPointMake(.5f + (sinf(angle) / 2.f), .5f - cosf(angle) / 2.f);
+}
+
+static CGPoint GRDEndPointForAngle (CGFloat angle){
+    return CGPointMake(.5f - (sinf(angle) / 2.f), .5f + (cosf(angle) / 2.f));
+}
+
 - (void)grd_changeGradient:(BOOL)animated{
     
     NSMutableArray *colours = [NSMutableArray array];
     for (NSUInteger i = 0; i < self.locations.count; i++) {
         [colours addObject:(id)[[UIColor grd_randomColor] CGColor]];
     }
+    
+    CGFloat angle = GRDRandomAngle();
+    CGPoint newStartPoint = GRDStartPointForAngle(angle);
+    CGPoint newEndPoint = GRDEndPointForAngle(angle);
+    
     if (animated) {
         CABasicAnimation *colorAnimation = [CABasicAnimation animationWithKeyPath:@"colors"];
         colorAnimation.fromValue = self.colors;
         colorAnimation.toValue = colours;
         colorAnimation.duration = kAnimationDuration;
         [self addAnimation:colorAnimation forKey:@"changeColors"];
+        CABasicAnimation *startPointAnimation = [CABasicAnimation animationWithKeyPath:@"startPoint"];
+        startPointAnimation.fromValue = [NSValue valueWithCGPoint:self.startPoint];
+        startPointAnimation.toValue = [NSValue valueWithCGPoint:newStartPoint];
+        startPointAnimation.duration = kAnimationDuration;
+        [self addAnimation:startPointAnimation forKey:@"changeStartPoint"];
+        CABasicAnimation *endPointAnimation = [CABasicAnimation animationWithKeyPath:@"endPoint"];
+        startPointAnimation.fromValue = [NSValue valueWithCGPoint:self.endPoint];
+        startPointAnimation.toValue = [NSValue valueWithCGPoint:newEndPoint];
+        startPointAnimation.duration = kAnimationDuration;
+        [self addAnimation:endPointAnimation forKey:@"changeEndPoint"];
     }
+    
+    self.startPoint = newStartPoint;
+    self.endPoint = newEndPoint;
     self.colors = colours;
+    
+}
+
+- (void)grd_rotate:(CGFloat)angle{
+    self.startPoint = GRDStartPointForAngle(angle);
+    self.endPoint = GRDEndPointForAngle(angle);
 }
 
 @end
